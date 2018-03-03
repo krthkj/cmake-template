ed41d50d3d93cce18e2a059e00cedf27ea1fd447
Karthik Jain <karthikjain@gmail.com>
2018-03-03 02:01:16 -0800

    update file header

--- MathFunctions/MakeTable.cpp
+++ MathFunctions/MakeTable.cpp
@@ -1,30 +1,29 @@
 #include "config.h"
 #include <cmath>
 
-#include <iostream>
-#include <cstdlib>
+#include <fstream>
 
 int main (int argc, char *argv[]){
-    std::cout<< argv[0]<<" version is "<<justDoIt_VERSION_MAJOR<<"."<<justDoIt_VERSION_MINOR<<std::endl;
 
     if (argc<2){
         return 1;
     }
 
     // create a source file with a table of square roots
-    FILE *fout = fopen(argv[1],"w");
-    if (!fout){
+    std::ofstream fout;
+    fout.open(argv[1]);
+    if (!fout.is_open()){
         return 1;
     }
     int num = atoi(argv[1]);
     double result=0;
-    fprintf(fout,"double sqrtTable[] = {\n");
+    fout << "double sqrtTable[] = {\n";
     for (int i = 0; i < num; ++i){
         result = sqrt(static_cast<double>(i));
-        fprintf(fout,"%g,\n",result);
+        fout<<result<<",\n";
     }
     // close the table with a zero
-    fprintf(fout,"0};\n");
-    fclose(fout);
+    fout<<"0};\n";
+    fout.close();
     return 0;
 }
