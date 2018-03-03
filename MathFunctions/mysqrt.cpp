734936fba0f8f428ae9f17f1e015ea92bea56cb2
Karthik Jain <karthikjain@gmail.com>
2018-03-03 01:10:48 -0800

    adding system introspection

--- MathFunctions/mysqrt.cpp
+++ MathFunctions/mysqrt.cpp
@@ -1,5 +1,12 @@
 #include <cmath>
 //#include "MathFunctions.h"
 double mysqrt(double x){
-    return sqrt(x);
+    double result;
+#if defined (HAVE_LOG) && defined (HAVE_EXP)
+    result = exp(log(x)*0.5);
+#else // otherwise use an iterative approach
+    result = sqrt(x);
+    // result = exp(log(x)*0.5);
+#endif
+    return result;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,53 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cstdio>
 
 int main (int argc, char *argv[]){
     std::cout<< argv[0]<<" version is "<<justDoIt_VERSION_MAJOR<<"."<<justDoIt_VERSION_MINOR<<std::endl;
+#ifdef HAVE_PRINTF
+    std::cout<<"floolwing is printf()"<<std::endl;
+    printf("This is my prntf command\n");
+#else
+std::cout<<"no printf"<<std::endl;
+
+#endif
+
     if (argc<2){
         std::cout<< "Usage: "<<argv[0]<<" number"<<std::endl;
     }
     else{
         double inVal = atof(argv[1]);
         // if (inVal<0) inVal*=-1;
+
+#ifdef HAVE_PRINTF
+        std::cout<< "HAVE_PRINTF Enable"<< std::endl;
+#else
+        std::cout<< "HAVE_PRINTF disable"<< std::endl;
+#endif
+
+#ifdef HAVE_EXP
+        std::cout<< "HAVE_EXP Enable"<< std::endl;
+#else
+        std::cout<< "HAVE_EXP disable"<< std::endl;
+#endif
+
+#ifdef HAVE_LOG
+        std::cout<< "HAVE_LOG Enable"<< std::endl;
+#else
+        std::cout<< "HAVE_LOG disable"<< std::endl;
+#endif
+#ifdef HAVE_MEMSET
+        std::cout<< "HAVE_MEMSET Enable"<< std::endl;
+#endif
 #ifdef USE_MYMATH
+
+        std::cout<< "MyMAth Enable"<< std::endl;
         double outVal = mysqrt(inVal);
-        std::cout<< "Using myMath"<<std::endl;
 #else
         double outVal = sqrt(inVal);
 #endif
+
         std::cout<<"The square root of "<< inVal<< " is "<<outVal <<std::endl;
     }
     return 0;
